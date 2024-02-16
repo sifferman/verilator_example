ec3886ae564cc73aa1112d4c11a996ce7665433d
Ethan Sifferman <ethan@sifferman.dev>
2024-02-15 18:36:36 -0800

    added verilator.f

---
+++ verilator.f
@@ -0,0 +1,25 @@
+
+lint.vlt
+test.sv
+tb.sv
+
+// Strict warnings
+-Wall
+// Don't exit on warnings
+-Wno-fatal
+
+// Fully parallelized
+-j 0
+// enable SystemVerilog assertions
+--assert
+// enable timing constructs
+--timing
+// dump as FST (compressed version of VCD)
+--trace-fst
+// dump structs as human-readable format
+--trace-structs
+
+// all explicit Xs are replaced by a constant value determined at runtime
+--x-assign unique
+// all variables are randomly initialized (if +verilator+rand+reset+2)
+--x-initial unique
